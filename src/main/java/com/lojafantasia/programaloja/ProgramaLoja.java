986ddf0b95d87e7d6b8c528704aef835b90a0877
Vinicius Santos Lima <viniciusslsilva2@gmail.com>
2023-10-23 01:30:54 -0300

    Primeiro Commit

---
+++ src/main/java/com/lojafantasia/programaloja/ProgramaLoja.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lojafantasia.programaloja;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class ProgramaLoja {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
